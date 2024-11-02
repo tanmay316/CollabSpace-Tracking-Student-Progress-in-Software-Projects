c7865c8b52fcec86d58c9af4e030fca34b8a59bd
anirudh <anirudhgr23@gmail.com>
2024-11-02 12:33:38 +0530

    Intialized database and imported required libraries for backend

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,7 @@
 from flask import Flask, jsonify
+from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
+from flask_cors import CORS
 
 from models import *
 from routes.student_routes import student
@@ -8,10 +11,21 @@ from routes.admin_routes import admin
 
 app = Flask(__name__)
 
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///LMS_v2_DB.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.app_context().push()
+db.init_app(app)
+migrate = Migrate(app, db)
+
 app.register_blueprint(student, url_prefix="/api")
 app.register_blueprint(instructor, url_prefix="/api")
 app.register_blueprint(ta, url_prefix="/api")
 app.register_blueprint(admin, url_prefix="/api")
+
+app.config['JWT_SECRET_KEY'] = os.urandom(24)
+jwt = JWTManager(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/models.py
+++ backend/models.py
@@ -2,3 +2,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Users(db.Model):
+    id = db.Column(db.Integer(), primary_key = True)
+    first_name = db.Column(db.String(), nullable = False)
+    last_name = db.Column(db.String())
+    email = db.Column(db.String(), nullable = False, unique = True)
+    password_hash = db.Column(db.String(), nullable = False) 
+    role = db.Column(db.String(), nullable = False)
+    
+'''
+- could add github profile link to users table
+
+project (GitHub link as one of the attributes), mentorship sessions, milestones, student doubts(for TA), feedback on project, viva slots, AI (need to figure out exact config),   
+'''
