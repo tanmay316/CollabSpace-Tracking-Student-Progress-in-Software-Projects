fdbc5e93a9aaa53dbf71ffb55b051feaf226f3d0
anirudh <anirudhgr23@gmail.com>
2024-11-02 12:45:52 +0530

    Added comments for backend config on app.py

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,38 @@
+# Import required frameworks/libraries
 from flask import Flask, jsonify
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_cors import CORS
 
+# Import necessary files and functions from the project
 from models import *
 from routes.student_routes import student
 from routes.instructor_routes import instructor
 from routes.TA_routes import ta
 from routes.admin_routes import admin
 
+# Initialize app
 app = Flask(__name__)
 
+# Setup CORS
 CORS(app)
 
+# Database config
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///LMS_v2_DB.sqlite3"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.app_context().push()
 db.init_app(app)
+
+# Initialize Flask migrate (allows to make changes to DB directly without deleting each time)
 migrate = Migrate(app, db)
 
+# Defines blueprints for routes based on users
 app.register_blueprint(student, url_prefix="/api")
 app.register_blueprint(instructor, url_prefix="/api")
 app.register_blueprint(ta, url_prefix="/api")
 app.register_blueprint(admin, url_prefix="/api")
 
+# JWT intializing for authentication
 app.config['JWT_SECRET_KEY'] = os.urandom(24)
 jwt = JWTManager(app)
 
