2a60aa71ea2e31ae27ad922d7a8dab3c796fda82
RajPatelIITM <rajpatel231103@gmail.com>
2024-12-05 18:53:39 +0530

    Chart Part majorly done

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,54 +1,59 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path:'/register',
-      name:'register',
-      component: () => import('../views/RegisterView.vue')
+      path: "/register",
+      name: "register",
+      component: () => import("../views/RegisterView.vue"),
     },
     {
-      path:'/login',
-      name:'login',
-      component: () => import('../views/LoginView.vue')
+      path: "/login",
+      name: "login",
+      component: () => import("../views/LoginView.vue"),
     },
     {
-      path:'/',
-      name:'project',
-      component: () => import('../views/ProjectView.vue')
+      path: "/",
+      name: "project",
+      component: () => import("../views/ProjectView.vue"),
     },
     {
-      path:'/milestones',
-      name:'milestone',
-      component: () => import('../views/MilestoneView.vue')
+      path: "/milestones",
+      name: "milestone",
+      component: () => import("../views/MilestoneView.vue"),
     },
     {
-      path:'/submission',
-      name:'submission',
-      component: () => import('../views/MilestoneSubmissionFeedbackView.vue')
+      path: "/submission",
+      name: "submission",
+      component: () => import("../views/MilestoneSubmissionFeedbackView.vue"),
     },
     {
-      path:'/mentorship',
-      name:'mentorship',
-      component: () => import('../views/MentorshipView.vue')
+      path: "/mentorship",
+      name: "mentorship",
+      component: () => import("../views/MentorshipView.vue"),
     },
     {
-      path:'/chatUsers',
-      name:'chatUsers',
-      component: () => import('../views/ChatUsersView.vue')
+      path: "/chatUsers",
+      name: "chatUsers",
+      component: () => import("../views/ChatUsersView.vue"),
     },
     {
-      path: '/summaryai',
-      name: 'summaryai',
-      component: () => import('../views/SummaryAI.vue'),
+      path: "/summaryai",
+      name: "summaryai",
+      component: () => import("../views/SummaryAI.vue"),
     },
     {
-      path:'/chatInterface',
-      name:'chatInterface',
-      component: () => import('../views/ChatInterfaceView.vue')
-    }
-  ]
-})
+      path: "/chatInterface",
+      name: "chatInterface",
+      component: () => import("../views/ChatInterfaceView.vue"),
+    },
+    {
+      path: "/adminDashboard",
+      name: "admin-dashboard",
+      component: () => import("../views/AdminPage.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
