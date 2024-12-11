835e9d8950956a707655fda54c7aba84026c4ce8
tanmay316 <sharmamoni913@gmail.com>
2024-12-11 19:26:15 +0530

    db

--- frontend/src/components/ChatUsers.vue
+++ frontend/src/components/ChatUsers.vue
@@ -7,7 +7,7 @@
         <img :src="user.image || 'https://via.placeholder.com/40'" alt="User Image" class="user-image" />
         <div class="user-info">
           <p class="user-name">{{ user.name }}</p>
-          <p class="user-last-msg">{{ user.lastMessage || 'No messages yet' }}</p>
+          <p class="user-last-msg">{{ user.lastMessage  }}</p>
         </div>
       </li>
     </ul>
