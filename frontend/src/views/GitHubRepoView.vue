283666443d013024f68963dd7654c74fb9e7b3ec
Anurag Sachan <connect2anurags@gmail.com>
2024-12-12 15:17:24 +0530

    ui: github repo feature v2

---
+++ frontend/src/views/GitHubRepoView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import GitHubRepo from '../components/GitHubRepo.vue';
+</script>
+
+<template>
+  <div class="GitHubRepo">
+    <main>
+      <GitHubRepo/>
+    </main>
+  </div>
+</template>
